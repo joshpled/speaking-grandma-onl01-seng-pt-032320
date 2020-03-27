2ce4d7ab41db28637f7a0685281482661d0e38c9
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-03-27 01:53:09 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -8,7 +8,7 @@ elsif phrase == phrase.upcase
   puts "NO, NOT SINCE 1938!"
 elsif phrase == "I LOVE YOU GRANDMA!"
   puts "I LOVE YOU TOO PUMPKIN!"
-end 
+end
 end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
