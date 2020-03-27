f8c79c1d48e93fea57ec097df55e52e03f6a7d22
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-03-27 02:01:23 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,9 +2,9 @@
 
 def speak_to_grandma(phrase)
 
-if phrase == (phrase.downcase || phrase.capitalize)
+if phrase == phrase.downcase && !("I LOVE YOU GRANDMA!")
   return "HUH?! SPEAK UP, SONNY!"
-elsif phrase == phrase.upcase
+elsif phrase == phrase.upcase && !("I LOVE YOU GRANDMA!")
   return "NO, NOT SINCE 1938!"
 elsif phrase == "I LOVE YOU GRANDMA!"
   return "I LOVE YOU TOO PUMPKIN!"
